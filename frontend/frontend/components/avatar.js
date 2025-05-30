b93452a307a615deac11bc94a0cd025edabc8707
teenSpirit <kll78460@gmail.com>
2025-05-31 00:43:16 +0800

    update

--- frontend/frontend/components/avatar.js
+++ frontend/frontend/components/avatar.js
@@ -1,13 +1,19 @@
-export default function Avatar() {
+export default function Avatar( props ) {
+  let username = props.username ? props.username: "Default User";
   return (
     <>
-      <div className="flex -space-x-2 overflow-hidden">
+      <div className="bg-white p-5 rounded-lg shadow">
+        <h3 className="text-lg font-semibold text-gray-700 mb-3">{username}</h3>
+        <div className="flex -space-x-2 overflow-hidden">
         <img
           alt=""
           src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          className="inline-block size-8 rounded-full ring-2 ring-white"
+          className="inline-block size-10 rounded-full ring-2 ring-white"
         />
       </div>
+        
+      </div>
+      
     </>
   )
 }
--- frontend/frontend/components/layout.js
+++ frontend/frontend/components/layout.js
@@ -3,9 +3,9 @@ import Head from "next/head";
 
 // import styles from "./layout.module.css"; 
 
-export default function Layout({ children, pageTitle = "Social Media Project" }) { 
+export default function Layout({ children, pageTitle}) { 
     return (
-        <div className="flex flex-col min-h-screen bg-gray-100"> {/* 1. 設定為 flex 容器，垂直排列，最小高度為螢幕高度，加上一個淺灰色背景 */}
+        <div className="flex flex-col min-h-screen bg-[#F2F2F2]"> {/* 1. 設定為 flex 容器，垂直排列，最小高度為螢幕高度，加上一個淺灰色背景 */}
             <Head>
                 <link rel="icon" href="/favicon.ico" />
                 <meta name="description" content="Social media project for cloud computing course" />
@@ -29,6 +29,7 @@ export default function Layout({ children, pageTitle = "Social Media Project" })
             {/* 5. 為 footer 加上樣式，並確保文字居中 */}
             <footer className="p-4 text-center mt-auto ">
                 <p className="text-sm">&copy; 2025 Copywrite Social Media Project. All rights reserved.</p>
+                
             </footer>
         </div>
     );
